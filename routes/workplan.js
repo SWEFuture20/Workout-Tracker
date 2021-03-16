32765a72c1ac719bb0ec54a02a9f039d6b0cbe97
Latifah Lillie <queen1892@gmail.com>
2021-03-16 18:09:41 -0500

    Creating get route to display data, get route to training results page, post to create data entered, and a put route to update an id and record an excercise

---
+++ routes/workplan.js
@@ -0,0 +1,36 @@
+const db = require("../models");
+
+module.exports = function(app) {
+
+    app.get("/api/training", (req, res) => {
+        db.Workout.find({}).then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+    })
+    app.get("/api/training/results", ({}, res) => {
+        db.Workout.find({}).then((dbWorkout) => {
+          res.json(dbWorkout);
+        }).catch(err => {
+          res.status(400).json(err);
+        });
+      });
+      app.post("/api/training/", (req, res) => {
+        db.Workout.create(req.body).then((dbWorkout) => {
+          res.json(dbWorkout);
+        }).catch(err => {
+            res.status(400).json(err);
+          });
+      });
+      app.put("/api/training/:id", (req, res) => {
+        db.Workout.findByIdAndUpdate(
+          { _id: req.params.id }, { exercises: req.body }
+        ).then((dbWorkout) => {
+          res.json(dbWorkout);
+        }).catch(err => {
+          res.status(400).json(err);
+        });
+    });
+};
